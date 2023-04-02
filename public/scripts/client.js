6a1895d52c9295e6cfb86b474a4cab678a186ab2
Medhanie Alene <alenemedhanie@gmail.com>
2023-04-01 20:56:27 -0400

    do some cleaning

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,20 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function () {
+$(document).ready(function() {
   loadTweets();
-  $("#tweet-check").hide();
-  $("#char-check").hide();
+  $("#tweet-check").hide();//empty tweet handler
+  $("#char-check").hide();//character exceeding tweet handler
 
-  $(".tweet-form").submit(function (event) {
+  $(".tweet-form").submit(function(event) {
     event.preventDefault();
     const serializedTweet = $(this).serialize();
+    //empty tweet check
     if ($("#tweet-text").val() === "") {
       $("#tweet-check").show();
       $("#tweet-check").html("**Empty tweets cannot be submitted!");
       $("#tweet-check").css("color", "red");
-    }
-    else if ($("#count-down").val() < 0) {
+      //check for tweets exceeding permitted character count
+    } else if ($("#count-down").val() < 0) {
       $("#char-check").show();
       $("#char-check").html("**Tweet should not exceed 140 characters!");
       $("#char-check").css("color", "red");
@@ -28,51 +29,53 @@ $(document).ready(function () {
           $("#tweet-text").val("");
           $("#tweet-check").hide();
           $("#char-check").hide();
-        })
+        });
     }
   });
 
-})
+});
 
+//tweet html
+const createTweetElement = function(tweetData) {
+  const tweetHTML =
+  `<article>
+    <header class="tweet-header"> 
+      <div> 
+        <img class="avatar" alt='profile-picture' src='${tweetData.user.avatars}' /> 
+        ${tweetData.user.name}
+      </div>
+      <h3>${tweetData.user.handle}</h3>
+    </header>
+    <textarea>${tweetData.content.text}</textarea>
+    <div class="line"></div>
+    <footer>
+        <span>${timeago.format(tweetData.created_at)}</span>
+        <div class="icons">
+          <i id="flag" class="fa-solid fa-flag"></i>
+          <i id="retweet" class="fa-solid fa-retweet"></i> 
+          <i id="heart" class="fa-solid fa-heart"></i>
+        </div>
+    </footer>
+  </article>`;
 
-const createTweetElement = function (tweetData) {
-  const tweetHTML = `<article>
-        <header class="tweet-header"> 
-          <div> 
-            <img class="avatar" alt='profile-picture' src='${tweetData.user.avatars}' /> 
-            ${tweetData.user.name}
-          </div>
-          <h3>${tweetData.user.handle}</h3>
-        </header>
-        <textarea>${tweetData.content.text}</textarea>
-        <div class="line"></div>
-        <footer>
-            <span>${timeago.format(tweetData.created_at)}</span>
-            
-            <div class="icons">
-              <i id="flag" class="fa-solid fa-flag"></i>
-              <i id="retweet" class="fa-solid fa-retweet"></i> 
-              <i id="heart" class="fa-solid fa-heart"></i>
-            </div>
-        </footer>
-      </article>`;
+  return tweetHTML;
+};
 
-  return tweetHTML
-}
+//display tweets
+const renderTweets = function(tweets) {
 
-const renderTweets = function (tweets) {
-
-  const section = $('.tweet-element')
+  const section = $('.tweet-element');
   for (const data of tweets) {
-    const tweetElement = createTweetElement(data)
-
+    const tweetElement = createTweetElement(data);
+    //show latest tweet on top
     section.prepend(tweetElement);
   }
-}
+};
 
-const loadTweets = function () {
+//tweet post
+const loadTweets = function() {
   $.ajax('/tweets', { method: 'GET' })
-    .then(function (tweets) {
+    .then(function(tweets) {
       renderTweets(tweets);
     });
 };
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,21 @@
-
-$(document).ready(function () {
+//composer character counter handling
+$(document).ready(function() {
   const textarea = document.getElementById("tweet-text");
   const maxCharacterAllowed = 140;
-  let characterLeft = 0;
-  $(textarea).on("input", function () {
+  let characterLeft = 0;//counter decreases as text is input
+  $(textarea).on("input", function() {
     const text = $(this).val();
-    console.log(this);
     if (text.length > maxCharacterAllowed) {
       characterLeft = maxCharacterAllowed - text.length;
     } else {
       characterLeft = maxCharacterAllowed - text.length;
-    }
+    } //counter changes color if character exceeds max
     if (characterLeft < 0) {
       $(".counter").html(characterLeft);
       $(".counter").css("color", "red");
-    } else {
+    } else {//counter color reverts to black as text is deleted
       $(".counter").html(characterLeft);
       $(".counter").css("color", "rgb(67, 66, 66)");
     }
-  })
+  });
 });
